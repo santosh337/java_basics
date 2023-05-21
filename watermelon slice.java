219609eb360b7365c45adf8083658cf5dde4e1be
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-21 06:21:22 +0000

    Create watermelon slice.java

---
+++ watermelon slice.java
@@ -0,0 +1,8 @@
+Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        if (n%2==0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
