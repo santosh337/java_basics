0d2888a0db7e0085e0818b27c1a5b3c12b234667
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-22 12:03:37 +0000

    Create print A to Z.java

---
+++ page9/print A to Z.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        char a = 'a';
+               int convert = (char)(a);
+               for (int i = convert; i<=(convert+25);i++) {
+                   char c = (char)(i);
+                   System.out.println(c);
+               }
+    }
+}
