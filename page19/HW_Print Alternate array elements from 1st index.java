377c109fbf5c5f3bb9c708f7ac9ab11fb4612199
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-23 09:36:29 +0000

    Create HW_Print Alternate array elements from 1st index.java

---
+++ page19/HW_Print Alternate array elements from 1st index.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a[]= new int[n]; 
+        for (int j=0;j<n;j++) {
+            int t = scn.nextInt();
+            a[j] = t;
+        }
+        
+        for (int i=0;i<a.length;i++) {
+            if (i%2==1) {
+                System.out.print(a[i] + " "); 
+            }
+        }
+    }
+}
