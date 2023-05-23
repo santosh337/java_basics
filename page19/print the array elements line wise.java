51ff7187734a3359ef4783aa534a63cbd8f6def9
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-23 09:34:22 +0000

    Create print the array elements line wise.java

---
+++ page19/print the array elements line wise.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a[]= new int[n]; 
+        for (int j=0;j<n;j++) {
+            int t = scn.nextInt();
+            a[j] = t;
+        }
+        
+        for (int i=0;i<a.length;i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
