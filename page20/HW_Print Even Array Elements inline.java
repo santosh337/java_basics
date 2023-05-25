9287239b331b34150bc549edd4b6024cee993c8e
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-25 06:22:28 +0000

    Create HW_Print Even Array Elements inline.java

---
+++ page20/HW_Print Even Array Elements inline.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] A = new int[n];
+        
+        for (int i=0;i<n;i++) {
+            A[i] = scn.nextInt();
+        }
+        
+        for (int j =0;j< n;j++) {
+            if (A[j]%2==0) {
+                System.out.print(A[j]+" ");
+            }
+        }
+    }
+}
