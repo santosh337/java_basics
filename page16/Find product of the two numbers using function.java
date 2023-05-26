ced9893740f0347186095790a9c1895cbf7adc83
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-26 15:16:47 +0000

    Create Find product of the two numbers using function.java

---
+++ page16/Find product of the two numbers using function.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Own Function
+    public static int findProduct(int x, int y) {
+        int ans = x * y;
+        return ans;
+    }
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        
+        for(int i  = 1; i <= t; i++){
+            int x = scn.nextInt();
+            int y = scn.nextInt();
+            System.out.println(findProduct(x , y));
+        }
+    }
+}
