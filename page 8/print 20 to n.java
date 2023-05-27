5d71eacdf38982a50bd1f94fcb48f830cc3d4ca5
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:11:58 +0000

    Create print 20 to n.java

---
+++ page 8/print 20 to n.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        
+        for (int j = 20 ; j<=num;j++) {
+        System.out.print(j+" ");
+        }
+    }
+}
