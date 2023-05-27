e6c918befb05b73181b10f7c3f2b254ad40562de
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:08:41 +0000

    Create Print 3 7 11 15...java

---
+++ page 7/Print 3 7 11 15...java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int input = scn.nextInt();
+        int number = 3;
+        for (int j = 0 ; j<input ;j++) {
+            if (number < input) {
+                System.out.println(number);
+                number += 4;
+            }
+        }
+    }
+}
