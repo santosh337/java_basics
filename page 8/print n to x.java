fead56e26b0c2c67c09c5b4a6220122dda9c5d83
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:10:27 +0000

    Create print n to x.java

---
+++ page 8/print n to x.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int x = scn.nextInt();
+        for (int j = num ; j>=x;j--) {
+        System.out.println(j);
+        }
+    }
+}
