ee5d1802556e166524d83e4e22af8132228f308e
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:07:46 +0000

    Create multiple of 7 .java

---
+++ page 7/multiple of 7 .java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int input = scn.nextInt();
+        for (int j = 0 ; j<=input ;j++) {
+                System.out.print(j+ " ");
+                j += 6;
+        }
+    }
+}
