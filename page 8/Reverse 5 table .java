77f48928dc27b8398360e7f3d107d00d7b093754
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:10:59 +0000

    Create Reverse 5 table .java

---
+++ page 8/Reverse 5 table .java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int num = 5;
+        for (int j = 10 ; j>0;j--) {
+        System.out.println(num + "x" + j + "=" + 5*j);
+        }
+    }
+}
