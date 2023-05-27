3db1ca903356307207b3e574b528deff616502c9
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:09:48 +0000

    Create print n to 0.java

---
+++ page 8/print n to 0.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        for (int j = num ; j>=0;j--) {
+        System.out.println(j);
+        }
+    }
+}
