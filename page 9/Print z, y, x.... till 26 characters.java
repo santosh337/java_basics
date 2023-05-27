d7ff8f1576fedd9307f8e67a5b3a02fbb14ea29d
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-27 14:20:12 +0000

    Create Print z, y, x.... till 26 characters.java

---
+++ page 9/Print z, y, x.... till 26 characters.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int convert = (char)('z');
+               for (int i = convert; i>=(char)('a');i--) {
+                   char c = (char)(i);
+                System.out.println(c);
+               }
+    }
+}
