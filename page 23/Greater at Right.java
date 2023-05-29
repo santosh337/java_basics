d27eaa62ea57391d5710bfaf02d78dd0ea500baa
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-29 15:57:38 +0000

    Create Greater at Right.java

---
+++ page 23/Greater at Right.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        //input for A array
+        int [] A = new int[n];
+        for(int i = 0; i < n; i++){
+            A[i] = scn.nextInt();
+        }
+    
+         
+        for (int j = 0;j<n;j++) { 
+            int sum = 0;
+            for (int l= j+1;l<n;l++) {
+                if (A[j] < A[l]) {
+                    sum += 1;
+                }
+            }
+            System.out.print(sum +" ");
+        }
+    }
+}
