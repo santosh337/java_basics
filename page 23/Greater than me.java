493f1462dddf7b5bae69e931f08537d665353a60
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-29 15:57:15 +0000

    Create Greater than me.java

---
+++ page 23/Greater than me.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] A = new int[n];
+        
+        for (int i=0;i<n;i++) {
+            A[i] = scn.nextInt();
+        }
+        
+        for(int j=0;j<n;j++) {
+            int sum = 0;
+            for (int k=0;k<n;k++) {
+                if (A[k] > A[j]) {
+                    sum += 1;
+                }
+            }
+            System.out.print(sum + " ");
+        }
+        
+    }
+}
