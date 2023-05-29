125984156a5b4f82e591cdc000c675978caaa8cc
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-29 15:59:13 +0000

    Create count odd pair.java

---
+++ page 23/count odd pair.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] A = new int[n];
+        
+        for (int i=0;i<n;i++) {
+            A[i] = scn.nextInt();
+        }
+        
+        for(int j=0;j<n;j++) {
+            for (int l=0;l<n;l++) {
+                if ((A[j] + A[l])%2== 1) {
+                    System.out.print(A[j]+" "+A[l]);
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
