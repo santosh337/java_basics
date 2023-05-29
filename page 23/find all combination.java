4d24cbd3a019cf62121985257c3fcbdb10168f3e
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-29 15:56:43 +0000

    Create find all combination.java

---
+++ page 23/find all combination.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        //input for A array
+        int [] A = new int[n];
+        for(int i = 0; i < n; i++){
+            A[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+    
+        for (int j = 0;j<n;j++) { 
+            for (int l = j;l< n;l++) {
+                if (A[j] + A[l] == k) {
+                    System.out.print(A[j] + " "+ A[l]);
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
