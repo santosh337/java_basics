f66c0cbd7a74af92e1ab6b6975ee71b7aebd30fb
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-30 12:05:38 +0000

    Create HW_Find Difference 2.java

---
+++ page 24/HW_Find Difference 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+       
+        for (int i=0;i<n;i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        int k = scn.nextInt();
+       
+        for (int j=0;j<n;j++) {
+            for (int l= 0;l<n;l++) {
+                if ((arr[j] - arr[l]) == -k) {
+                    System.out.println(arr[j]+" "+ arr[l]);
+                }
+            }
+        }
+    }
+}
