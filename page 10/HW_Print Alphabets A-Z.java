de13559037f133fb962583a4b27e00d0543bd83f
Santosh Sugur <santosh.iosdev@gmail.com>
2023-06-09 18:14:04 +0000

    Create HW_Print Alphabets A-Z.java

---
+++ page 10/HW_Print Alphabets A-Z.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+               int convert = (char)('A');
+               for (int i = convert; i<=(convert+25);i++) {
+                   char c = (char)(i);
+                   System.out.print(c+" ");
+               }
+    }
+}
