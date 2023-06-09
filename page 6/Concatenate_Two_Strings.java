d345b9889786257ee3083864d650bcef9eb512af
Santosh Sugur <santosh.iosdev@gmail.com>
2023-06-09 18:11:51 +0000

    Create Concatenate_Two_Strings.java

---
+++ page 6/Concatenate_Two_Strings.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String first = scn.nextLine();
+        String second = scn.nextLine();
+        String concate = first + second;
+        
+        System.out.print(concate);
+    }
+}
