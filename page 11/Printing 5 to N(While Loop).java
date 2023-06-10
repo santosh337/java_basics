0c95ee4ef4d90fb7ee10598cd7d78b76a8cd9b8d
Santosh Sugur <santosh.iosdev@gmail.com>
2023-06-10 05:27:07 +0000

    Create Printing 5 to N(While Loop).java

---
+++ page 11/Printing 5 to N(While Loop).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int i=5;
+        while (i<=num) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
