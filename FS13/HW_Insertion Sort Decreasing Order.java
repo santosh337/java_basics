b3471b2854929ef7aa638412414d786df2b5245f
Santosh Sugur <santosh.iosdev@gmail.com>
2023-06-18 17:59:23 +0000

    Create HW_Insertion Sort Decreasing Order.java

---
+++ FS13/HW_Insertion Sort Decreasing Order.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        for (int i =1;i<n;i++) {
+            for (int j=i-1;j>=0;j--) {
+                if (arr[j] < arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+    
+        for (int k=0;k<n;k++) {
+            System.out.print(arr[k]+" ");
+        }
+    }
+}
